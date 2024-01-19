c5c7a19d865770c0db8c7a3362cfdf8eb08add65
tomy07417 <tamundarain@fi.uba.ar>
2024-01-19 18:49:05 -0300

    project 04 first solution

---
+++ packages/04-Prueba-Tecnica/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './App.css'
+
+const URL_API_FACTS = "https://catfact.ninja/fact"
+const URL_CAT_ROOT = "https://cataas.com"
+
+function App() {
+  const [fact, setFact] = useState(false)
+  const [urlCat, setUrlCat] = useState(false)
+
+  useEffect(() => {
+    fetch(URL_API_FACTS)
+      .then(response => response.json())
+      .then(data => {
+        setFact(data.fact)
+        const word = data.fact.split(' ',3).join()
+        fetch(`https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red&json=true`)
+          .then(response => response.json())
+          .then(data => {
+            const {_id} = data
+            setUrlCat(`/cat/${_id}/says/${word}`)
+          })
+      })
+      
+  }, [])
+
+  return (
+    <>
+      <h1>Prueba Tecnica</h1>
+      {fact && <p>{fact}</p>}
+      {urlCat && <img src={URL_CAT_ROOT+urlCat}/>}
+    </>
+  )
+}
+
+export default App
